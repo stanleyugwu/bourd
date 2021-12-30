a8ae8849dcb41c8c1227ccb3b334f5e47a06d4df
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-12-30 23:22:37 +0100

    feat: add `ScreensNavigationParams` type
    This type defines the screens for the app, their names, and the route paramaters they take.
    
    Its to be edited when new screen is to be registered.

--- types.d.ts
+++ types.d.ts
@@ -1,10 +1,10 @@
 /** Type for a project after creation; with full details */
 export type Project = NewProject & {
-    /** A unique `alpah-num` ID for the project */
-    projectId:string;
-    /** Date project was created `(in milliseconds)` */
-    createdAt:LooseDate;
-}
+  /** A unique `alpah-num` ID for the project */
+  projectId: string;
+  /** Date project was created `(in milliseconds)` */
+  createdAt: LooseDate;
+};
 
 /** Less strict date type. Accepts either `Date` object or number in `milliseconds` */
 type LooseDate = Date | number;
@@ -14,51 +14,64 @@ type taskStatus = "to-do" | "doing" | "done";
 
 /** Type for a new project without app-added details*/
 type NewProject = {
-    /** What you'll call the project. A `catchy` name */
-    projectName:string;
-    /** A lengthy description of the project and what it does */
-    projectDescription: string;
-    /** An array of `ProjectTask` to be carried out for the project */
-    projectTasks: ProjectTask[];
-}
+  /** What you'll call the project. A `catchy` name */
+  projectName: string;
+  /** A lengthy description of the project and what it does */
+  projectDescription: string;
+  /** An array of `ProjectTask` to be carried out for the project */
+  projectTasks: ProjectTask[];
+};
 
 /** Described the type for the core properties of a project task */
 type ProjectTaskCore = {
-    /** Description of the task to be carried out */
-    taskDescription: string;
-    /** Text describing the state of the task 
-     * - `"to-do"` means task is **not** carried out
-     * - `"doing"` means task is **currently** being carried out
-     * - `"done"` means task is **completely** carried out
-    */
-    taskStatus: taskStatus;
-}
+  /** Description of the task to be carried out */
+  taskDescription: string;
+  /** Text describing the state of the task
+   * - `"to-do"` means task is **not** carried out
+   * - `"doing"` means task is **currently** being carried out
+   * - `"done"` means task is **completely** carried out
+   */
+  taskStatus: taskStatus;
+};
 
 /** Type for a project's task with full details */
 type ProjectTask = ProjectTaskCore & {
-    /** A numeric ID for task */
-    taskId:string;
-    /** Date of task creation in app `(in milliseconds)` */
-    createdAt: LooseDate
-}
+  /** A numeric ID for task */
+  taskId: string;
+  /** Date of task creation in app `(in milliseconds)` */
+  createdAt: LooseDate;
+};
 
 /** Payload type to dispatch when creating new project task */
 type AddProjectTaskPayload = ProjectTaskCore & {
-    /** `Id` of project that has the task */
-    projectId:string;
-}
+  /** `Id` of project that has the task */
+  projectId: string;
+};
 
 /** Payload type to dispatch when modifying project */
 type ModifyProjectPayload = {
-    /** `Id` of the project to be modified */
-    projectId:string;
-    /** modified name of the project */
-    projectName:string;
-    /** modified description of the project */
-    projectDescription:string
-}
+  /** `Id` of the project to be modified */
+  projectId: string;
+  /** modified name of the project */
+  projectName: string;
+  /** modified description of the project */
+  projectDescription: string;
+};
 
 type ModifyProjectTaskPayload = ProjectTaskCore & {
-    /** `Id` of project task to modify */
-    taskId:string;
-}
+  /** `Id` of project task to modify */
+  taskId: string;
+};
+
+type ProjectIdPayloadType = {
+  projectId: string;
+};
+
+type ScreensNavigationParams = {
+  Projects: undefined;
+  Project: ProjectIdPayloadType;
+  NewProject: undefined;
+  EditProject: ProjectIdPayloadType;
+  NewProjectTask: ProjectIdPayloadType;
+  EditProjectTask: ProjectIdPayloadType & {taskId: string}
+};
